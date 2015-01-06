caf8d2606bd68ae16bcebf4e5e160fc79c33653c
Ben Willers <bwillers@digisoft.tv>
2015-01-06 15:39:32 +0100

    add hostconfig fields to containerconfig

--- src/main/java/com/nirima/docker/client/model/ContainerConfig.java
+++ src/main/java/com/nirima/docker/client/model/ContainerConfig.java
@@ -2,6 +2,8 @@ package com.nirima.docker.client.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+
 import com.google.common.base.Objects;
 
 import java.io.Serializable;
@@ -30,6 +32,11 @@ public class ContainerConfig  implements Serializable {
     @JsonProperty("Env")          private String[]  env;
     @JsonProperty("Cmd")          private String[]  cmd;
 
+    @JsonSerialize(include= JsonSerialize.Inclusion.NON_NULL)
+    @JsonProperty("HostConfig")
+    private HostConfig hostConfig;
+    //@JsonProperty("HostConfig")   private HostConfig hostConfig;
+
     // Seems deprecated in later oocker APIs
     @JsonProperty("Dns")          private String[]  dns;
     @JsonProperty("Image")        private String    image;
@@ -44,6 +51,14 @@ public class ContainerConfig  implements Serializable {
     @JsonProperty("ExposedPorts")   private Map<String, ?> exposedPorts;
     
     @JsonProperty("OnBuild")   private int[] onBuild;
+
+    public HostConfig getHostConfig() {
+        return hostConfig;
+    }
+    public ContainerConfig setHostConfig(HostConfig hostConfig) {
+        this.hostConfig = hostConfig;
+        return this;
+    }
 
     public Map<String, ?> getExposedPorts() {
         return exposedPorts;
@@ -321,6 +336,7 @@ public class ContainerConfig  implements Serializable {
                 .add("domainName", domainName)
                 .add("exposedPorts", exposedPorts)
                 .add("onBuild", onBuild)
+                .add("hostConfig", hostConfig)
                 .toString();
     }
 }
